c91470b7c77adc002c3e00f0f55def5cfd6d6db3
rastrakidyana <rastrakidyana@users.noreply.github.com>
2020-12-07 13:25:17 +0800

    nambah maps

--- src/components/User/TentangKami.vue
+++ src/components/User/TentangKami.vue
@@ -8,8 +8,8 @@
             </div>
 
             <div>
-                <h2> Tentang Kami </h2>
-                <h5 style="text-align : justify"> Couponic merupakan situs yang menyediakan fasilitas top-up dalam komunitas game dan
+                <h1 class="font-weight-bold mb-5"> Tentang Kami </h1>
+                <h5 style="text-align : justify" class="font-weight-bold mb-5"> Couponic merupakan situs yang menyediakan fasilitas top-up dalam komunitas game dan
                     penjualan akun premium di Indonesia. Kami mempunyai layanan yang menjamin 100% keamanan dan 
                     kemudahan bertransaksi karena aman dan praktis untuk digunakan. Kami akan selalu berusaha
                     memberikan fasilitas terbaik dan harga termurah untuk pelanggan. </h5>
@@ -19,10 +19,10 @@
                 <b-card no-body class="mx-auto" style="max-width: 900px; max-height: 500px;" elevation="10">
                     <b-row no-gutters>
                     <b-col md="6">
-                        <img src="@/assets/contactUs.jpg" alt="Image" class="rounded-0" style="width: 450px; height: 500px;">
+                        <iframe class="mt-8" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.2770253111815!2d110.40768602913928!3d-7.778361370884056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59ebffa226fb%3A0xc7b08c10552662b2!2sArtindo!5e0!3m2!1sid!2sid!4v1607318298571!5m2!1sid!2sid" width="400" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                     </b-col>
                     <b-col md="6">
-                        <b-card-body class="hubungiKami" title="Hubungi Kami">
+                        <b-card-body class="hubungiKami font-weight-medium" title="Hubungi Kami">
                         <b-card-text class="text-left" >
                             <v-form
                                 ref="form"
@@ -61,6 +61,7 @@
                     </b-col>
                     </b-row>
                 </b-card>
+
             </div>
         </v-main>
     </v-container>
